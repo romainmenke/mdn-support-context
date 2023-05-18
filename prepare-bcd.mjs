917a7a11687cab00cf1ed54a421263dbc2fd44b9
Romain Menke <romainmenke@gmail.com>
2023-05-18 16:44:23 +0200

    reduce size of the extension

---
+++ prepare-bcd.mjs
@@ -0,0 +1,78 @@
+import bcd from '@mdn/browser-compat-data' assert { type: 'json' };
+import fs from 'fs';
+
+const ignoreBrowsersMDN = /^(chrome_android|firefox_android|webview_android|oculus|nodejs)/i;
+
+function reduceDataFromBCD(obj) {
+	if (!obj) return;
+
+	delete obj.__meta;
+
+	if (obj.__compat) {
+		delete obj.__compat.mdn_url;
+		delete obj.__compat.source_file;
+		delete obj.__compat.spec_url;
+		delete obj.__compat.description;
+	}
+
+	if (obj.__compat && obj.__compat.support) {
+		let minimalSupport = {};
+		let support = obj.__compat.support;
+
+		for (const browser in support) {
+			if (ignoreBrowsersMDN.test(browser)) continue;
+
+			if (support[browser]) {
+				let versions = Array.isArray(support[browser]) ? support[browser] : [support[browser]];
+
+				for (let i = 0; i < versions.length; i++) {
+					let versionInfo = versions[i];
+					let versionAdded = versionInfo.version_added;
+					if (!versionAdded) continue;
+					if (versionInfo.flags) continue;
+					if (versionInfo.version_removed) continue;
+					if (versionInfo.prefix) continue;
+					if (versionInfo.alternative_name) continue;
+					if (versionInfo.partial_implementation) continue;
+
+					if (versionAdded === true) {
+						versionAdded = '1';
+					}
+
+					if (versionAdded.startsWith('≤')) {
+						versionAdded = versionAdded.substring(1);
+					}
+
+					if (minimalSupport[browser]) {
+						console.log(obj, 'minimalSupport[browser]', minimalSupport[browser]);
+					}
+
+					minimalSupport[browser] = {
+						version_added: versionAdded
+					};
+				}
+			}
+		}
+
+		obj.__compat.support = minimalSupport;
+	}
+
+	for (const key in obj) {
+		if (Object.prototype.hasOwnProperty.call(obj, key) === false) continue;
+
+		if (key === '__compat') continue;
+
+		if (typeof obj[key] === 'string') continue;
+		if (typeof obj[key] === 'number') continue;
+		if (typeof obj[key] === 'boolean') continue;
+
+		reduceDataFromBCD(obj[key]);
+	}
+}
+
+const bcdCopy = structuredClone(bcd);
+reduceDataFromBCD(bcdCopy);
+
+fs.writeFileSync('./src/bcd.js', `
+module.exports = ${JSON.stringify(bcdCopy, null)};
+`);
